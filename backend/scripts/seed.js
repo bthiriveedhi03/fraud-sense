6ce44b337fb25cb3f282e56d31658bf518c789e8
manishamishra230062-art <manisha.mishra.230062@gmail.com>
2026-09-12 15:26:18 -0500

    any backend modifs

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-const provider = require('../backend/dataProvider');
+const provider = require('../dataProvider');
 
 const NUM_CUSTOMERS = 15;
 const PURCHASES_PER_ACCOUNT = 40;
